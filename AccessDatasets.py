0a9ba2381a9f57552dc095be0cd42a3c5be9be56
Nandini-Kanawade <92260339+Nandini-Kanawade@users.noreply.github.com>
2023-03-22 01:11:24 +0530

    Accessing the Datasets.

---
+++ AccessDatasets.py
@@ -0,0 +1,8 @@
+# We will use 'pandas' Library for working with the dataSheet
+import pandas as A
+# Reading the file using read_csv() function and printing the datasheet
+dataSheet = A.read_csv('name_of_file.csv')
+print(dataSheet)
+# Reading the file using read_csv() function and printing the datasheet
+dataSheet = A.read_csv('disney_movies.csv')
+print(dataSheet)
